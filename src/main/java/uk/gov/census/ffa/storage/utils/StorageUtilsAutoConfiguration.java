a8203565784c705abd038ac02ec5fad8b83e366c
sdiaz-ons <simon.diaz@ext.ons.gov.uk>
2026-07-14 17:02:24 +0100

    Add Spring Boot auto-configuration metadata for storage utils

---
+++ src/main/java/uk/gov/census/ffa/storage/utils/StorageUtilsAutoConfiguration.java
@@ -0,0 +1,9 @@
+package uk.gov.census.ffa.storage.utils;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan(basePackages = "uk.gov.census.ffa.storage.utils")
+public class StorageUtilsAutoConfiguration {
+}
